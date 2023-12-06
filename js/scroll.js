e747f2db6840452e1865bd2224fbf7c558a8edd0
keferson-github <kefersonbuzatto90@gmail.com>
2023-12-06 18:24:31 -0300

    Implementação de Javascript para o scroll to top.

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+const scrollAnima = document.querySelector('[data-anima="scroll"]');
+
+const halfOfTheWindow = window.innerHeight * 4.5;
+
+function animationScroll() {
+    const topItem = scrollAnima.getBoundingClientRect().top;
+
+    const visibleItem = topItem - halfOfTheWindow < 0;
+
+    if (visibleItem) {
+        scrollAnima.classList.add('show-btn');
+    } else {
+        scrollAnima.classList.remove('show-btn');
+    };
+};
+
+window.addEventListener('scroll', animationScroll);
